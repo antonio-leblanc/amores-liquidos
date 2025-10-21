8b11d1fab06d964eead08e4c92581998eeee35ea
antonio-leblanc <antonioleblanc@poli.ufrj.br>
2025-10-20 21:48:38 -0300

    vamo

---
+++ song-data-final.js
@@ -0,0 +1,409 @@
+const songData = [
+  {
+    "id": "100_voce",
+    "title": "100 Voce"
+  },
+  {
+    "id": "acima_do_sol",
+    "title": "Acima Do Sol",
+    "melodies": {
+      "Trombone": "markdown/acima_do_sol_trombone.md"
+    }
+  },
+  {
+    "id": "alo_paixao",
+    "title": "Alo Paixao",
+    "melodies": {
+      "Trombone": "markdown/alo_paixao_trombone.md"
+    }
+  },
+  {
+    "id": "amor_de_chocolate",
+    "title": "Amor De Chocolate"
+  },
+  {
+    "id": "amor_i_love_you",
+    "title": "Amor I Love You",
+    "melodies": {
+      "Trombone": "markdown/amor_i_love_you_trombone.md"
+    }
+  },
+  {
+    "id": "amor_perfeito",
+    "title": "Amor Perfeito",
+    "melodies": {
+      "Trombone": "markdown/amor_perfeito_trombone.md"
+    }
+  },
+  {
+    "id": "andanca",
+    "title": "Andanca",
+    "melodies": {
+      "Trombone": "markdown/andanca_trombone.md"
+    }
+  },
+  {
+    "id": "assim_caminha_a_humanidade_malhacao",
+    "title": "Assim Caminha A Humanidade Malhacao",
+    "melodies": {
+      "Trombone": "markdown/assim_caminha_a_humanidade_malhacao_trombone.md"
+    }
+  },
+  {
+    "id": "baianidade_nago",
+    "title": "Baianidade Nago",
+    "melodies": {
+      "Trombone": "markdown/baianidade_nago_trombone.md"
+    }
+  },
+  {
+    "id": "bailao",
+    "title": "Bailao"
+  },
+  {
+    "id": "baile_da_gaiola",
+    "title": "Baile Da Gaiola"
+  },
+  {
+    "id": "beleza_rara",
+    "title": "Beleza Rara",
+    "melodies": {
+      "Trombone": "markdown/beleza_rara_trombone.md"
+    }
+  },
+  {
+    "id": "bola_de_sabao",
+    "title": "Bola De Sabao",
+    "melodies": {
+      "Trombone": "markdown/bola_de_sabao_trombone.md"
+    }
+  },
+  {
+    "id": "cilada",
+    "title": "Cilada",
+    "melodies": {
+      "Trombone": "markdown/cilada_trombone.md"
+    }
+  },
+  {
+    "id": "de_ladinho",
+    "title": "De Ladinho",
+    "melodies": {
+      "Trombone": "markdown/de_ladinho_trombone.md"
+    }
+  },
+  {
+    "id": "depois_do_prazer",
+    "title": "Depois Do Prazer",
+    "melodies": {
+      "Trombone": "markdown/depois_do_prazer_trombone.md"
+    }
+  },
+  {
+    "id": "deus_me_livre",
+    "title": "Deus Me Livre",
+    "melodies": {
+      "Trombone": "markdown/deus_me_livre_trombone.md"
+    }
+  },
+  {
+    "id": "deusa_do_amor",
+    "title": "Deusa Do Amor",
+    "melodies": {
+      "Trombone": "markdown/deusa_do_amor_trombone.md"
+    }
+  },
+  {
+    "id": "diga_que_valeu",
+    "title": "Diga Que Valeu"
+  },
+  {
+    "id": "do_seu_lado",
+    "title": "Do Seu Lado",
+    "melodies": {
+      "Trombone": "markdown/do_seu_lado_trombone.md"
+    }
+  },
+  {
+    "id": "dont_stop",
+    "title": "Dont Stop"
+  },
+  {
+    "id": "eva",
+    "title": "Eva",
+    "melodies": {
+      "Trombone": "markdown/eva_trombone.md"
+    }
+  },
+  {
+    "id": "exagerado",
+    "title": "Exagerado"
+  },
+  {
+    "id": "festa",
+    "title": "Festa"
+  },
+  {
+    "id": "fico_assim_sem_voce",
+    "title": "Fico Assim Sem Voce",
+    "melodies": {
+      "Trombone": "markdown/fico_assim_sem_voce_trombone.md"
+    }
+  },
+  {
+    "id": "final_feliz",
+    "title": "Final Feliz",
+    "melodies": {
+      "Trombone": "markdown/final_feliz_trombone.md"
+    }
+  },
+  {
+    "id": "flor_do_reggae",
+    "title": "Flor Do Reggae",
+    "melodies": {
+      "Trombone": "markdown/flor_do_reggae_trombone.md"
+    }
+  },
+  {
+    "id": "gatas_extraordinarias",
+    "title": "Gatas Extraordinarias",
+    "melodies": {
+      "Trombone": "markdown/gatas_extraordinarias_trombone.md"
+    }
+  },
+  {
+    "id": "hey_jude",
+    "title": "Hey Jude",
+    "melodies": {
+      "Trombone": "markdown/hey_jude_trombone.md"
+    }
+  },
+  {
+    "id": "i_love_you_baby_cant_take_my_eyes_off_you",
+    "title": "I Love You Baby Cant Take My Eyes Off You"
+  },
+  {
+    "id": "i_miss_her",
+    "title": "I Miss Her"
+  },
+  {
+    "id": "ja_sei_namorar",
+    "title": "Ja Sei Namorar",
+    "melodies": {
+      "Trombone": "markdown/ja_sei_namorar_trombone.md"
+    }
+  },
+  {
+    "id": "lua_de_cristal",
+    "title": "Lua De Cristal",
+    "melodies": {
+      "Trombone": "markdown/lua_de_cristal_trombone.md"
+    }
+  },
+  {
+    "id": "lucro",
+    "title": "Lucro",
+    "melodies": {
+      "Trombone": "markdown/lucro_trombone.md"
+    }
+  },
+  {
+    "id": "mal_acostumado",
+    "title": "Mal Acostumado",
+    "melodies": {
+      "Trombone": "markdown/mal_acostumado_trombone.md"
+    }
+  },
+  {
+    "id": "mania_de_voce",
+    "title": "Mania De Voce",
+    "melodies": {
+      "Trombone": "markdown/mania_de_voce_trombone.md"
+    }
+  },
+  {
+    "id": "maria_maria",
+    "title": "Maria Maria"
+  },
+  {
+    "id": "me_abraca_me_beija",
+    "title": "Me Abraca Me Beija",
+    "melodies": {
+      "Trombone": "markdown/me_abraca_me_beija_trombone.md"
+    }
+  },
+  {
+    "id": "me_deixa",
+    "title": "Me Deixa"
+  },
+  {
+    "id": "meia_lua",
+    "title": "Meia Lua"
+  },
+  {
+    "id": "meu_sangue_ferve_por_voce",
+    "title": "Meu Sangue Ferve Por Voce",
+    "melodies": {
+      "Trombone": "markdown/meu_sangue_ferve_por_voce_trombone.md"
+    }
+  },
+  {
+    "id": "milla",
+    "title": "Milla",
+    "melodies": {
+      "Trombone": "markdown/milla_trombone.md"
+    }
+  },
+  {
+    "id": "nao_precisa_mudar",
+    "title": "Nao Precisa Mudar",
+    "melodies": {
+      "Trombone": "markdown/nao_precisa_mudar_trombone.md"
+    }
+  },
+  {
+    "id": "nao_quero_dinheiro",
+    "title": "Nao Quero Dinheiro",
+    "melodies": {
+      "Trombone": "markdown/nao_quero_dinheiro_trombone.md"
+    }
+  },
+  {
+    "id": "pintura_intima",
+    "title": "Pintura Intima",
+    "melodies": {
+      "Trombone": "markdown/pintura_intima_trombone.md"
+    }
+  },
+  {
+    "id": "princesa",
+    "title": "Princesa",
+    "melodies": {
+      "Trombone": "markdown/princesa_trombone.md"
+    }
+  },
+  {
+    "id": "sina",
+    "title": "Sina",
+    "melodies": {
+      "Trombone": "markdown/sina_trombone.md"
+    }
+  },
+  {
+    "id": "sinais_de_fogo",
+    "title": "Sinais De Fogo"
+  },
+  {
+    "id": "so_love",
+    "title": "So Love",
+    "melodies": {
+      "Trombone": "markdown/so_love_trombone.md"
+    }
+  },
+  {
+    "id": "sombra_da_maldade",
+    "title": "Sombra Da Maldade"
+  },
+  {
+    "id": "sorte_grande",
+    "title": "Sorte Grande",
+    "melodies": {
+      "Trombone": "markdown/sorte_grande_trombone.md"
+    }
+  },
+  {
+    "id": "swing_da_cor",
+    "title": "Swing Da Cor",
+    "melodies": {
+      "Trombone": "markdown/swing_da_cor_trombone.md"
+    }
+  },
+  {
+    "id": "tambem_quero_beijar_flor_do_maracuja",
+    "title": "Tambem Quero Beijar Flor Do Maracuja",
+    "melodies": {
+      "Trombone": "markdown/tambem_quero_beijar_flor_do_maracuja_trombone.md"
+    }
+  },
+  {
+    "id": "temporal",
+    "title": "Temporal",
+    "melodies": {
+      "Trombone": "markdown/temporal_trombone.md"
+    }
+  },
+  {
+    "id": "tempos_modernos",
+    "title": "Tempos Modernos"
+  },
+  {
+    "id": "tieta",
+    "title": "Tieta"
+  },
+  {
+    "id": "to_nem_ai",
+    "title": "To Nem Ai"
+  },
+  {
+    "id": "toda_forma_de_amor",
+    "title": "Toda Forma De Amor",
+    "melodies": {
+      "Trombone": "markdown/toda_forma_de_amor_trombone.md"
+    }
+  },
+  {
+    "id": "tremendo_vacilao",
+    "title": "Tremendo Vacilao",
+    "melodies": {
+      "Trombone": "markdown/tremendo_vacilao_trombone.md"
+    }
+  },
+  {
+    "id": "tu_ta_na_gaiola",
+    "title": "Tu Ta Na Gaiola"
+  },
+  {
+    "id": "uma_noite_e_meia",
+    "title": "Uma Noite E Meia",
+    "melodies": {
+      "Trombone": "markdown/uma_noite_e_meia_trombone.md"
+    }
+  },
+  {
+    "id": "vai_novinha",
+    "title": "Vai Novinha"
+  },
+  {
+    "id": "vai_sacudir_vai_abalar",
+    "title": "Vai Sacudir Vai Abalar",
+    "melodies": {
+      "Trombone": "markdown/vai_sacudir_vai_abalar_trombone.md"
+    }
+  },
+  {
+    "id": "varias_queixas",
+    "title": "Varias Queixas"
+  },
+  {
+    "id": "vem_meu_amor",
+    "title": "Vem Meu Amor",
+    "melodies": {
+      "Trombone": "markdown/vem_meu_amor_trombone.md"
+    }
+  },
+  {
+    "id": "voa_voa",
+    "title": "Voa Voa"
+  },
+  {
+    "id": "voce_me_vira_a_cabeca",
+    "title": "Voce Me Vira A Cabeca"
+  },
+  {
+    "id": "voce_nao_entende_nada",
+    "title": "Voce Nao Entende Nada",
+    "melodies": {
+      "Trombone": "markdown/voce_nao_entende_nada_trombone.md"
+    }
+  }
+];
